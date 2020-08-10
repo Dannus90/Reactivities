ecdf048f14961bfb9fd701fd486229bac3d87352
Dannus90 <persson.daniel.1990@gmail.com>
2020-08-10 13:32:59 +0200

    Initial Commit

---
+++ Domain/Value.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Value
+    {
+        //A property within entities with ID -> Is automatically be set as the primary key. 
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
