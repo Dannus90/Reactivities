f476a0eb261e011e88bcd392f988886892fb45e6
Dannus90 <persson.daniel.1990@gmail.com>
2020-08-29 21:24:32 +0200

    Add React Widgets

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom";
 import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
 import "react-toastify/dist/ReactToastify.min.css";
+import 'react-widgets/dist/css/react-widgets.css';
+import dateFnsLocalizer from 'react-widgets-date-fns';
 import "./app/layout/styles.css";
 import App from "./app/layout/App";
 import * as serviceWorker from "./serviceWorker";
 import "semantic-ui-css/semantic.min.css";
 import ScrollToTop from "./app/layout/ScrollToTop";
 import "mobx-react-lite/batchingForReactDom";
+
+dateFnsLocalizer();
 
 export const history = createBrowserHistory();
 
