482bc5dd528a8abbb856507889ce55c05279b6e5
Dannus90 <persson.daniel.1990@gmail.com>
2020-09-12 20:50:23 +0200

    Add method for generating Jwt

--- Infrastructure/Security/JwtGenerator.cs
+++ Infrastructure/Security/JwtGenerator.cs
@@ -1,7 +1,13 @@
+using System;
+using System.Text;
 using System.Security.AccessControl;
 using System.Net.Mime;
 using Application.Interfaces;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
 using Domain;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Infrastructure.Security
 {
@@ -9,7 +15,27 @@ namespace Infrastructure.Security
   {
     public string CreateToken(AppUser user)
     {
-      throw new System.NotImplementedException();
+      var claims = new List<Claim>
+      {
+        new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+      };
+
+      // generate signing credentials
+      var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("super secret key"));
+      var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+      var tokenDescriptor = new SecurityTokenDescriptor
+      {
+        Subject = new ClaimsIdentity(claims),
+        Expires = DateTime.Now.AddDays(7),
+        SigningCredentials = creds
+      };
+
+      var tokenHandler = new JwtSecurityTokenHandler();
+
+      var token = tokenHandler.CreateToken(tokenDescriptor);
+
+      return tokenHandler.WriteToken(token);
     }
   }
 }
