b43c71cd8b133e4dcb47656ba072e7677a8b23c9
Daniel Persson <persson.daniel.1990@gmail.com>
2020-11-01 21:34:16 +0100

    Edit landing page and create dropdown in navbar

--- API/Startup.cs
+++ API/Startup.cs
@@ -24,82 +24,81 @@ using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace API
 {
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-      Configuration = configuration;
-    }
-
-    public IConfiguration Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddDbContext<DataContext>(opt =>
-     {
-       opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
-     });
-      services.AddCors(opt =>
-     {
-       opt.AddPolicy("CorsPolicy", policy =>
-             {
-               policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-             });
-     });
-      services.AddMediatR(typeof(List.Handler).Assembly);
-      services.AddControllers(opt =>
-      {
-        var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
-        opt.Filters.Add(new AuthorizeFilter(policy));
-      }).AddFluentValidation(cfg =>
-      {
-
-        cfg.RegisterValidatorsFromAssemblyContaining<Create>();
-      });
-
-      var builder = services.AddIdentityCore<AppUser>();
-      var identityBuilder = new IdentityBuilder(builder.UserType, builder.Services);
-      identityBuilder.AddEntityFrameworkStores<DataContext>();
-      identityBuilder.AddSignInManager<SignInManager<AppUser>>();
-
-      var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokenKey"]));
-      services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
-      {
-        opt.TokenValidationParameters = new TokenValidationParameters
+        public Startup(IConfiguration configuration)
         {
-          ValidateIssuerSigningKey = true,
-          IssuerSigningKey = key,
-          ValidateAudience = false,
-          ValidateIssuer = false,
-        };
-      });
+            Configuration = configuration;
+        }
 
-      services.AddScoped<IJwtGenerator, JwtGenerator>();
-      services.AddScoped<IUserAccessor, UserAccessor>();
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(opt =>
+           {
+               opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+           });
+            services.AddCors(opt =>
+           {
+               opt.AddPolicy("CorsPolicy", policy =>
+               {
+                       policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                   });
+           });
+            services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddControllers(opt =>
+            {
+                var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+                opt.Filters.Add(new AuthorizeFilter(policy));
+            }).AddFluentValidation(cfg =>
+            {
+                cfg.RegisterValidatorsFromAssemblyContaining<Create>();
+            });
+
+            var builder = services.AddIdentityCore<AppUser>();
+            var identityBuilder = new IdentityBuilder(builder.UserType, builder.Services);
+            identityBuilder.AddEntityFrameworkStores<DataContext>();
+            identityBuilder.AddSignInManager<SignInManager<AppUser>>();
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokenKey"]));
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+            {
+                opt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = key,
+                    ValidateAudience = false,
+                    ValidateIssuer = false,
+                };
+            });
+
+            services.AddScoped<IJwtGenerator, JwtGenerator>();
+            services.AddScoped<IUserAccessor, UserAccessor>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseMiddleware<ErrorHandlingMiddleware>();
+            if (env.IsDevelopment())
+            {
+                // app.UseDeveloperExceptionPage();
+            }
+            //Anything hitting http will automatically be converted to httpS
+            // app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseCors("CorsPolicy");
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+           {
+               endpoints.MapControllers();
+           });
+        }
     }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-      app.UseMiddleware<ErrorHandlingMiddleware>();
-      if (env.IsDevelopment())
-      {
-        // app.UseDeveloperExceptionPage();
-      }
-      //Anything hitting http will automatically be converted to httpS
-      // app.UseHttpsRedirection();
-
-      app.UseRouting();
-      app.UseCors("CorsPolicy");
-
-      app.UseAuthentication();
-      app.UseAuthorization();
-
-      app.UseEndpoints(endpoints =>
-     {
-       endpoints.MapControllers();
-     });
-    }
-  }
 }
